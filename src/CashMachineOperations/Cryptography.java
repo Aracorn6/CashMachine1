1812e52c57194a420b89e07e2a93f1e3f43fe19a
aracorn6 <semenchuktltsu@gmail.com>
2016-12-26 16:21:35 +0400

    v. 0.9.8

--- src/CashMachineOperations/Cryptography.java
+++ src/CashMachineOperations/Cryptography.java
@@ -7,23 +7,22 @@ import java.security.NoSuchAlgorithmException;
  * Created by aracorn on 25.12.16.
  */
 public class Cryptography {
-        public static String encrypt(String pin)
-        {
-            String encryptPin="";
-            try {
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(pin.getBytes());
+    public static String encrypt(String pin) {
+        String encryptPin = "";
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(pin.getBytes());
 
-                byte byteData[] = md.digest();
-                StringBuffer sb = new StringBuffer();
-                for (int i = 0; i < byteData.length; i++) {
-                    sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
-                }
-
-                encryptPin=sb.toString();
-            } catch (NoSuchAlgorithmException ex) {
-                ex.printStackTrace();
+            byte byteData[] = md.digest();
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < byteData.length; i++) {
+                sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
             }
-            return encryptPin;
+
+            encryptPin = sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            ex.printStackTrace();
         }
+        return encryptPin;
+    }
 }
